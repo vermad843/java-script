c257f845a17bd85e94e893b1fee32a6bc8cd013f
vermad843 <vishnuverma7524@gmail.com>
2019-10-17 19:57:14 +0530

    working with if ,else if,else

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// difference between '==' and '===' 
-// ===   10 = '10' => not equal;
+// if ,else if , else  
 
-
-function compareEquality(a , b) {
-  if(a == b) {
-    return " equal";
+function testSize(num) {
+  if(num <5) {
+    return "tiny";
+  }else if(num <10) {
+    return "small";
+  }else if(num < 15){
+    return "medium";
+  }else if(num <20) {
+    return "large";
+  }else {
+    return "huge";
   }
-  return " not Equal";
 }
 
-console.log(compareEquality(10, '10'));
+console.log(testSize(4));
+console.log(testSize(5));
+console.log(testSize(11));
+console.log(testSize(19));
+console.log(testSize(125));
