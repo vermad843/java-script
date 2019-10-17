769659b9b7ec3fda4904bc476f707f56e1f458b1
vermad843 <vishnuverma7524@gmail.com>
2019-10-17 20:18:38 +0530

    writing same code using if,else if ,else and switch

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-// if ,else if , else  
 
-function testSize(num) {
-  if(num <5) {
-    return "tiny";
-  }else if(num <10) {
-    return "small";
-  }else if(num < 15){
-    return "medium";
-  }else if(num <20) {
-    return "large";
+// Switch can be used in place of if,else if ,else
+
+function animalNames(val) {
+  let answer = "";
+  switch(val) {
+    case "a":
+      answer = "apes";
+      break;
+    case "b":
+      answer = "bird";
+      break;  
+    case "c":
+      answer = "cat";
+      break;
+    case "d":
+      answer = "dog";
+      break;  
+    default: 
+      answer = "stuff";
+      break;       
+  }
+  return answer;
+}
+
+
+function animalNames(val) {
+  if(val == "a") {
+    return "apes";        
+  }else if(val =="b" ) {
+   return "bird";
+  }else if(val == "c") {
+    return "cat";
+  }else if(val == "d") {
+    return "dog";
   }else {
-    return "huge";
+    return "stuff";
   }
 }
 
-console.log(testSize(4));
-console.log(testSize(5));
-console.log(testSize(11));
-console.log(testSize(19));
-console.log(testSize(125));
+
+
+
+
+
+
+
+
+
+console.log(animalNames("a"));
+console.log(animalNames("b"));
+console.log(animalNames("c"));
+console.log(animalNames("d"));
+console.log(animalNames("e"));
+
