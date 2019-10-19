4efed28a8bfd0a5ab5e66658b38e2564e2ee705a
vermad843 <vishnuverma7524@gmail.com>
2019-10-19 10:17:20 +0530

    while loop

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// objects 
-//accessing the object property using dot(.) and square([]) notation; 
-//[""] 
+// while loop 
 
 
-let football = {
-  "name" : "C.Ronaldo",                   //property
-  "player no" : "7",
-  "team" : "juventus",
-  "previous team" : "real madrid",
-  "sentence" : function() {                              //method
-    return `${football.name} wear jersey number ${football["player no"]} and he is playing for ${football.team} and he 
-     previously played for ${football["previous team"]}.`
-  } 
-};
+let myArray = [];
 
-const playerName = football.name;
-console.log(playerName);
+let i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++ ;
+}
 
-const playerNo = football["player no"];
-console.log(playerNo);
-
-const fullSentence = football.sentence();
-console.log(fullSentence);
+console.log(myArray);
