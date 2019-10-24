52c3bc546626e6fc285808ca3c0574fe4d431ecd
vermad843 <vishnuverma7524@gmail.com>
2019-10-24 21:58:12 +0530

    toUpperCase()

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
-// sorting the Array(Ascending)
+// use of toUpperCase()
 
-let array1 = [5, 12, 8, 130, 44];
+let sentence = 'Cristiano Ronaldo';
 
-array1.sort(function(a, b) {
-    return a - b 
-});
-
-console.log(array1);
-//expected output [5,8,12,44,130]
-
-
-
-// sorting the Array(Descending)
-
-let array2 = [5, 12, 8, 130, 44];
-
-
-array2.sort(function(a, b) {
-    return b - a  
-});
-
-console.log(array2);
-//expected output [130,44,12,8,5]
-
-
-
-
-//sorting alphabetically
-var fruits = ["Banana", "Orange", "Apple", "Mango"];
-fruits.sort();   
-
-console.log(fruits);
-
-
-//reversing the sorted Array
-fruits.reverse();
-
-console.log(fruits)
-
-
-
-
-
+console.log(sentence.toUpperCase());
+// CRISTIANO RONALDO
