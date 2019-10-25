e239fd052a0987bd4ee838c417a7127738e61da4
vermad843 <vishnuverma7524@gmail.com>
2019-10-25 18:29:15 +0530

    use of Math.pow(number, 5)

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // use of **   : 2 to the power 5 ;
+// Math.pow(number, 5)
 
 const betterNumbers = [2, 13, 42];
-const squaredNumber = betterNumbers.map((number) => {
-    return number**5; 
+// const squaredNumber = betterNumbers.map((number) => {
+//     return number**5; 
+// });
+
+
+const squaredNumber = betterNumbers.map(number => {
+  return Math.pow(number,5);
 });
 
 console.log(squaredNumber);
+
+
+
