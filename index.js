a0c43dd0374c71b23b5d8aca7b9bd65d2554b82a
vermad843 <vishnuverma7524@gmail.com>
2019-10-25 18:14:25 +0530

    new operator()

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 //hasOwnProperty() : it is giving a boolean value:
 
 
-
-const object = new Object();
+const object = new Object('number');
 object.number = 42;
 
 console.log(object.hasOwnProperty('number'));
 // expected output: true
 
+object.toString = 'hey';
 console.log(object.hasOwnProperty('toString'));
-// expected output: false
+// expected output: true
 
 console.log(object.hasOwnProperty('hasOwnProperty'));
 // expected output: false
+
+
+// new operator : Creates a blank, plain JavaScript object;
+
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+var car = new Car('Eagle', 'Talon TSi', 1993);
+
+console.log(car.make);
+// expected output: "Eagle"
+
