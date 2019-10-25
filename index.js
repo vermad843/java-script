4e6da6bf070022a8da9fbb9dbca610f2562bb09d
vermad843 <vishnuverma7524@gmail.com>
2019-10-25 18:22:57 +0530

    use of **

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-//hasOwnProperty() : it is giving a boolean value:
+// use of **   : 2 to the power 5 ;
 
+const betterNumbers = [2, 13, 42];
+const squaredNumber = betterNumbers.map((number) => {
+    return number**5; 
+});
 
-const object = new Object('number');
-object.number = 42;
-
-console.log(object.hasOwnProperty('number'));
-// expected output: true
-
-object.toString = 'hey';
-console.log(object.hasOwnProperty('toString'));
-// expected output: true
-
-console.log(object.hasOwnProperty('hasOwnProperty'));
-// expected output: false
-
-
-// new operator : Creates a blank, plain JavaScript object;
-
-
-function Car(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-}
-
-var car = new Car('Eagle', 'Talon TSi', 1993);
-
-console.log(car.make);
-// expected output: "Eagle"
-
+console.log(squaredNumber);
