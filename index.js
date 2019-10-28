db9fee8ce4c42fdf196e766f5f5186532539e0e4
vermad843 <vishnuverma7524@gmail.com>
2019-10-28 20:56:58 +0530

    comparing binary values

--- index.js
+++ index.js
@@ -263,3 +263,16 @@ console.log(5 & 13); // 0101 & 1101 = 0101
 console.log(5 | 13); // 0101 | 1101 = 1101
 // expected output: 13
 
+
+console.log(parseInt("0101",2) & parseInt("1101",2));
+// expected output: 5;
+
+console.log(5 & 13 & 3); // 0101 & 1101 & 0011 = 0001
+// expected output: 1;
+
+
+// comparing 0101 & 1101
+// 1 : true
+// 0 : false 
+// 1 & 0 => 0(false)
+//  1 | 0 =>1(true) 
