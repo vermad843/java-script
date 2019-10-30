7ce960e50cf36c8cee17dc732dd7410ff8b4c5d4
vermad843 <vishnuverma7524@gmail.com>
2019-10-30 14:44:51 +0530

    endWith problem

--- index.js
+++ index.js
@@ -302,31 +302,38 @@
 // Math.max()
 
 
-console.log(Math.max(1, 3, 2));
-// expected output: 3
+// console.log(Math.max(1, 3, 2));
+// // expected output: 3
 
-console.log(Math.max(-1, -3, -2));
-// expected output: -1
+// console.log(Math.max(-1, -3, -2));
+// // expected output: -1
 
-var array1 = [1, 3, 2];
+// var array1 = [1, 3, 2];
 
-console.log(Math.max(...array1));
-// expected output: 3
+// console.log(Math.max(...array1));
+// // expected output: 3
 
 
 
-// Math.min()
+// // Math.min()
 
-console.log(Math.min(2, 3, 1));
-// expected output: 1
+// console.log(Math.min(2, 3, 1));
+// // expected output: 1
 
-console.log(Math.min(-2, -3, -1));
-// expected output: -3
+// console.log(Math.min(-2, -3, -1));
+// // expected output: -3
 
-var array1 = [2, 3, 1];
+// var array1 = [2, 3, 1];
 
-console.log(Math.min(...array1));
-// expected output: 1
+// console.log(Math.min(...array1));
+// // expected output: 1
+
+
+// endsWith()
+
+let str = "Hello world, welcome to the universe.";
+let n = str.endsWith("universe.");
+console.log(n);//true
 
 
 
