48dc2a5f4ede99c41a6f796067a5728fd0f6ab4c
vermad843 <vishnuverma7524@gmail.com>
2019-10-30 11:09:40 +0530

    Math.min()

--- index.js
+++ index.js
@@ -312,3 +312,21 @@ var array1 = [1, 3, 2];
 
 console.log(Math.max(...array1));
 // expected output: 3
+
+
+
+// Math.min()
+
+console.log(Math.min(2, 3, 1));
+// expected output: 1
+
+console.log(Math.min(-2, -3, -1));
+// expected output: -3
+
+var array1 = [2, 3, 1];
+
+console.log(Math.min(...array1));
+// expected output: 1
+
+
+
